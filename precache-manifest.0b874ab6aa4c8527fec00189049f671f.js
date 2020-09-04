8fd4fe39ae996c56278cde2ce34f17cb9a21b050
Vojtěch Štěpán <vojtech.stepan@cgi.com>
2020-09-04 12:21:35 +0200

    Updates

---
+++ precache-manifest.0b874ab6aa4c8527fec00189049f671f.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bbec2df2c7b1ed0693b634ee92497e37",
+    "url": "/Calculator/index.html"
+  },
+  {
+    "revision": "585ad1fad4fa7ece8c7e",
+    "url": "/Calculator/static/css/main.2d58a1b8.chunk.css"
+  },
+  {
+    "revision": "6b00792279431f955118",
+    "url": "/Calculator/static/js/2.21e34db5.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/Calculator/static/js/2.21e34db5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "585ad1fad4fa7ece8c7e",
+    "url": "/Calculator/static/js/main.a3ca1626.chunk.js"
+  },
+  {
+    "revision": "0a79fa1d2af7875576c8",
+    "url": "/Calculator/static/js/runtime-main.8483c221.js"
+  },
+  {
+    "revision": "0fd1680fec973d2844a2f53d4addfcfc",
+    "url": "/Calculator/static/media/roi-desktop.0fd1680f.png"
+  },
+  {
+    "revision": "d615a594ba0a3f605250a9168d882cf2",
+    "url": "/Calculator/static/media/roi-mobile.d615a594.png"
+  }
+]);
